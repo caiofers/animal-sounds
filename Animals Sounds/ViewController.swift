8dde2014a35235c28b59724ca1f3cdd88aa3b23b
Caio <caiovr13@gmail.com>
2021-04-15 23:09:28 -0300

    Initial Commit

---
+++ Animals Sounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animals Sounds
+//
+//  Created by Caio Fernandes on 15/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
