6912311ceec69b81e594f235e0921f17c0253b59
Caio <caiovr13@gmail.com>
2021-04-15 23:42:46 -0300

    Lógica dos sons dos animais

--- Animals Sounds/ViewController.swift
+++ Animals Sounds/ViewController.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBAction func playDog(_ sender: Any) {
+        playSound(soundName: "cao")
+    }
+    
+    @IBAction func playLion(_ sender: Any) {
+        playSound(soundName: "leao")
+    }
+    
+    @IBAction func playSheep(_ sender: Any) {
+        playSound(soundName: "ovelha")
+    }
+    
+    @IBAction func playCat(_ sender: Any) {
+        playSound(soundName: "gato")
+    }
+    
+    @IBAction func playMonkey(_ sender: Any) {
+        playSound(soundName: "macaco")
+    }
+    
+    @IBAction func playCow(_ sender: Any) {
+        playSound(soundName: "vaca")
+    }
+    
+    func playSound(soundName: String) {
+        if let path = Bundle.main.path(forResource: soundName, ofType: "mp3") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player.prepareToPlay()
+                player.play()
+            } catch {
+                
+            }
+        }
+    }
+    
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
